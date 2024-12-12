aa072f123fb68abdb3d919abb65eebb336aa8570
Kang-Yi-Hun <rkddlgns1110@gmail.com>
2024-12-12 18:28:45 +0900

    끝

---
+++ SEMI/src/main/java/member/model/MemberDAO.java
@@ -0,0 +1,13 @@
+package member.model;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import member.domain.MemberVO;
+
+public interface MemberDAO {
+
+   // 모든 회원을 조회하는 메소드
+   List<MemberVO> SelectAll_member() throws SQLException;
+   
+}
